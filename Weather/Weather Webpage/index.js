18820d2e3f4e0e000acce455bcfd032cf988964e
Christian Pari <ChristianPari@outlook.com>
2020-05-03 19:34:41 -0400

    Functionality is 100%

--- Weather/Weather Webpage/index.js
+++ Weather/Weather Webpage/index.js
@@ -1,13 +1,3 @@
-/*
-APIs
-current weather
-https://api.openweathermap.org/data/2.5/weather?appid={your api key}&q={query}
-
-historical weather
-https://api.meteostat.net/v1/stations/search?key=${api Key}&q=${city Name} gets weather stations
-https://api.meteostat.net/v1/history/daily?key=${api Key}&station=${station id}&start=${start Date}&end=${end Date} gets weather data
-*/
-
 let owKey = `c81d51bf76bfdeb0cf59fa68e2336eb5`,
     metKey = `UcUSKkeI`,
     body = document.body,
@@ -127,19 +117,34 @@ function curStart() {
 
 function displayCurData(curData) {
 
+    let xhr = new XMLHttpRequest(),
+        endpoint = curData.sys.country.length > 2 ? `https://restcountries.eu/rest/v2/alpha/${curData.sys.country}` : `https://restcountries.eu/rest/v2/alpha/${curData.sys.country}`;
+
+    xhr.open('GET', endpoint, false);
+
+    xhr.onload = () => {
+
+        let res = JSON.parse(xhr.responseText),
+            location = `${curData.name}, ${res.name}`;
+
+        curData.name = location;
+
+    };
+
+    xhr.send();
+
     let locationDiv = createDiv({ id: ``, class: `locationDivs` }),
-        locationName = `${curData.name}, ${curData.sys.country}`,
-        location = createHeading({ text: locationName, size: 3, id: ``, class: `locations` }),
+        location = createHeading({ text: curData.name, size: 3, id: ``, class: `locations` }),
         weatherInfoDiv = createDiv({ id: ``, class: `infoDivs` }),
         weatherCondDiv = createDiv({ id: ``, class: `conditionDivs` }),
-        sun = createHeading({ text: `Sunrise: ${convertUnix(curData.sys.sunrise)} AM / Sunset: ${convertUnix(curData.sys.sunset)} PM`, size: 4, class: `conditions`, id: `` }),
-        humidity = createHeading({ text: `Humidity: ${Math.round(curData.main.humidity)}%`, size: 4, id: ``, class: `conditions` }),
-        feelsLike = createHeading({ text: `Feels Like: ${Math.round(curData.main.feels_like)}°F`, size: 4, id: ``, class: `conditions` }),
-        curTemp = createHeading({ text: `Current Temperature: ${Math.round(curData.main.temp)}°F`, size: 4, id: ``, class: `conditions` }),
-        tempRange = createHeading({ text: `High: ${Math.round(curData.main.temp_max)}°F / Low: ${Math.round(curData.main.temp_min)}°F`, size: 4, id: ``, class: `conditions` }),
-        pressure = createHeading({ text: `Air Pressure: ${Math.round(curData.main.pressure)} hPa`, size: 4, id: ``, class: `conditions` }),
-        windSpeed = createHeading({ text: `Wind Speed: ${Math.round(curData.wind.speed)}mph / Gusts: ${Math.round(curData.wind.gust)}mph`, size: 4, class: `conditions`, id: `` }),
-        windDirection = createHeading({ text: `Wind Direction: ${curData.wind.deg}°`, class: `conditions`, id: ``, size: 4 }),
+        sun = createHeading({ text: `Sunrise: ${convertUnix(curData.sys.sunrise)} AM / Sunset: ${convertUnix(curData.sys.sunset)} PM`, size: 4, class: `curConditions`, id: `` }),
+        humidity = createHeading({ text: `Humidity: ${Math.round(curData.main.humidity)}%`, size: 4, id: ``, class: `curConditions` }),
+        feelsLike = createHeading({ text: `Feels Like: ${Math.round(curData.main.feels_like)}°F`, size: 4, id: ``, class: `curConditions` }),
+        curTemp = createHeading({ text: `Current Temperature: ${Math.round(curData.main.temp)}°F`, size: 4, id: ``, class: `curConditions` }),
+        tempRange = createHeading({ text: `High: ${Math.round(curData.main.temp_max)}°F / Low: ${Math.round(curData.main.temp_min)}°F`, size: 4, id: ``, class: `curConditions` }),
+        pressure = createHeading({ text: `Air Pressure: ${Math.round(curData.main.pressure)} hPa`, size: 4, id: ``, class: `curConditions` }),
+        windSpeed = createHeading({ text: `Wind Speed: ${Math.round(curData.wind.speed)}mph / Gusts: ${Math.round(curData.wind.gust)}mph`, size: 4, class: `curConditions`, id: `` }),
+        windDirection = createHeading({ text: `Wind Direction: ${curData.wind.deg}°`, class: `curConditions`, id: ``, size: 4 }),
         conditionsImg = createImage({ src: `http://openweathermap.org/img/wn/${curData.weather[0].icon}@2x.png`, alt: `weather icon image`, id: ``, class: `icons` }),
         deleteButton = createButton({ text: `X`, onClickFunc: deleteDiv, class: `deleteButtons`, id: `` });
 
@@ -219,8 +224,6 @@ function histStart() {
 
 function stationFilter(stations) {
 
-    console.log(stations);
-
     if (stations.length == 0) {
 
         alert(`No weather stations found, please try another search`);
@@ -232,10 +235,7 @@ function stationFilter(stations) {
 
     } else {
 
-        // code for multiple stations
-        // create select
-        // create cancel button
-        // assign choosen station to the singleStation function
+        multipleStations(stations);
 
     }
 
@@ -264,14 +264,292 @@ function singleStation(station) {
 
 };
 
-function getHistData(stationInfo) {
+function multipleStations(wStations) {
 
-    console.log(stationInfo);
+    wStations.forEach(station => {
+
+        let xhr = new XMLHttpRequest(),
+            endpoint = station.country.length > 2 ? `https://restcountries.eu/rest/v2/alpha/${station.country}` : `https://restcountries.eu/rest/v2/alpha/${station.country}`;
+
+        xhr.open('GET', endpoint, false);
+
+        xhr.onload = () => {
+
+            let res = JSON.parse(xhr.responseText),
+                location = `${station.name}, ${res.name}`;
+
+            station.name = location; // combines name, easier for later
+
+        };
+
+        xhr.send();
+
+    });
+
+    if (document.getElementById(`stationSelect`) != null) { document.getElementById(`stationSelect`).remove() };
+    if (document.getElementById(`cancelButton`) != null) { document.getElementById(`cancelButton`).remove() };
+
+    let histUiDiv = document.getElementById(`histUiDiv`),
+        stationSelect = document.createElement(`select`),
+        cancelButton = createButton({ id: `cancelButton`, text: `Cancel`, onClickFunc: cancelProcess });
+
+    stationSelect.id = `stationSelect`;
+    stationSelect.size = wStations.length;
+
+    for (let a = 0; a < wStations.length; a++) {
+
+        console.log(wStations[a]);
+
+        let option = document.createElement(`option`);
+
+        option.innerHTML = wStations[a].name;
+        option.value = wStations[a].id;
+
+        stationSelect.appendChild(option);
+
+    }
+
+    histUiDiv.appendChild(stationSelect);
+    histUiDiv.appendChild(cancelButton);
+    document.getElementById(`yearSelect`).style.display = `none`;
+    document.getElementById(`monthSelect`).style.display = `none`;
+    document.getElementById(`daySelect`).style.display = `none`;
+    document.getElementById(`histInput`).style.display = `none`;
+    document.getElementById(`histButton`).style.display = `none`;
+
+    stationSelect.onchange = () => {
+
+        getHistData({ id: stationSelect.value, name: stationSelect.options[stationSelect.selectedIndex].text });
+        document.getElementById(`stationSelect`).style.display = `none`;
+        document.getElementById(`cancelButton`).style.display = `none`;
+        document.getElementById(`yearSelect`).style.display = `initial`;
+        document.getElementById(`monthSelect`).style.display = `initial`;
+        document.getElementById(`daySelect`).style.display = `initial`;
+        document.getElementById(`histInput`).style.display = `initial`;
+        document.getElementById(`histButton`).style.display = `initial`;
+
+    }
 
 };
+
+function getHistData(stationInfo) {
+
+    let xhr = new XMLHttpRequest(),
+        month = dateInfo.month < 10 ? `0` + dateInfo.month : dateInfo.month,
+        day = dateInfo.day < 10 ? `0` + dateInfo.day : dateInfo.day,
+        startDate = `${dateInfo.year}-${month}-${day}`,
+        endpoint = `https://api.meteostat.net/v1/history/daily?key=${metKey}&station=${stationInfo.id}&start=${startDate}&end=${startDate}`;
+
+    xhr.open('GET', endpoint);
+
+    xhr.onload = () => {
+
+        let res = JSON.parse(xhr.responseText);
+        displayHistData(res.data[0], stationInfo.name);
+
+    };
+
+    xhr.send();
+
+};
+
+function displayHistData(histData, stationName) {
+
+    if (histData != null) {
+
+        let stationDiv = createDiv({ class: `stationDivs`, id: `` }),
+            weatherDiv = createDiv({ class: `weatherDivs`, id: `` }),
+            nameHead = createHeading({ text: stationName, size: 3, id: ``, class: `stationNames` });
+
+        weatherDiv.appendChild(nameHead);
+
+        for (const k in histData) {
+
+            if (histData[k] != null) {
+
+                let converted = convertData(k, histData[k]),
+                    element = createHeading({ text: converted, class: `histConditions`, id: ``, size: 4 });
+
+                weatherDiv.appendChild(element);
+
+            }
+
+        }
+
+        let deleteButton = createButton({ text: `X`, onClickFunc: deleteDiv, class: `deleteButtons`, id: `` });
+
+        stationDiv.appendChild(deleteButton);
+        stationDiv.appendChild(weatherDiv);
+        document.getElementById(`histDataDiv`).appendChild(stationDiv);
+
+    } else {
+
+        alert(`Unfortunately there is no weather data for this day`);
+        return
+
+    }
+
+};
+
+function convertData(key, value) {
+
+    switch (key) {
+
+        case `peakgust`:
+
+            return `Wind Gusts: ${Math.round(value / 1.609344)} mph`
+
+        case `windspeed`:
+
+            let speed = Math.round(value / 1.609344) < 1 ? (value / 1.609344).toFixed(2) : Math.round(value / 1.609344);
+
+            return `Max Wind Speed: ${speed} mph`
+
+        case `precipitation`:
+
+            return `Rainfall total: ${Math.round(value / 25.4)} in`
+
+        case `pressure`:
+
+            return `Air Pressure: ${value.toFixed(2)} hPa`
+
+        case `snowdepth`:
+
+            return `Snow Depth: ${Math.round(value / 2.54)} in`
+
+        case `snowfall`:
+
+            return `Snowfall total: ${Math.round(value / 2.54)} in`
+
+        case `temperature`:
+
+            return `Average Temperature: ${Math.round((value * 9 / 5) + 32)}°F`
+
+        case `temperature_max`:
+
+            return `High Temperature: ${Math.round((value * 9 / 5) + 32)}°F`
+
+        case `temperature_min`:
+
+            return `Low Temperature: ${Math.round((value* 9 / 5) + 32)}°F`
+
+        case `winddirection`:
+
+            return getWindDirection(value)
+
+        case `date`:
+
+            return convertDate(value)
+
+        case `sunshine`:
+
+            return `Sunshine for: ${value} hours`
+
+    }
+
+};
+
+function getWindDirection(value) {
+    let windDir = value,
+        direction = ``;
+
+    if (windDir < 22.5) {
+
+        direction = `Wind Direction: N ${windDir}°`;
+
+    } else if (windDir >= 22.5 && windDir < 45) {
+
+        direction = `Wind Direction: NNE ${windDir}°`;
+
+    } else if (windDir >= 45 && windDir < 67.5) {
+
+        direction = `Wind Direction: NE ${windDir}°`;
+
+    } else if (windDir >= 67.5 && windDir < 90) {
+
+        direction = `Wind Direction: ENE ${windDir}°`;
+
+    } else if (windDir >= 90 && windDir < 112.5) {
+
+        direction = `Wind Direction: E ${windDir}°`;
+
+    } else if (windDir >= 112.5 && windDir < 135) {
+
+        direction = `Wind Direction: ESE ${windDir}°`;
+
+    } else if (windDir >= 135 && windDir < 157.5) {
+
+        direction = `Wind Direction: SE ${windDir}°`;
+
+    } else if (windDir >= 157.5 && windDir < 180) {
+
+        direction = `Wind Direction: SSE ${windDir}°`;
+
+    } else if (windDir >= 180 && windDir < 202.5) {
+
+        direction = `Wind Direction: S ${windDir}°`;
+
+    } else if (windDir >= 202.5 && windDir < 225) {
+
+        direction = `Wind Direction: SSW ${windDir}°`;
+
+    } else if (windDir >= 225 && windDir < 247.5) {
+
+        direction = `Wind Direction: SW ${windDir}°`;
+
+    } else if (windDir >= 247.5 && windDir < 270) {
+
+        direction = `Wind Direction: WSW ${windDir}°`;
+
+    } else if (windDir >= 270 && windDir < 292.5) {
+
+        direction = `Wind Direction: W ${windDir}°`;
+
+    } else if (windDir >= 292.5 && windDir < 315) {
+
+        direction = `Wind Direction: WNW ${windDir}°`;
+
+    } else if (windDir >= 315 && windDir < 337.5) {
+
+        direction = `Wind Direction: NW ${windDir}°`;
+
+    } else if (windDir >= 337.5) {
+
+        direction = `Wind Direction: NNW ${windDir}°`;
+
+    }
+
+    return direction
+
+};
+
+function convertDate(value) {
+
+    value = value.replace(/-/g, ` `).split(` `);
+    let dateArr = [];
+
+    value.forEach(data => {
+        dateArr.push(Number(data));
+    });
+
+    return `${monthNames[dateArr[1]-1]} ${dateArr[2]}, ${dateArr[0]}`
+
+}
 
 function deleteDiv() {
 
     this.parentNode.remove();
+
+};
+
+function cancelProcess() {
+
+    document.getElementById(`stationSelect`).style.display = `none`;
+    document.getElementById(`cancelButton`).style.display = `none`;
+    document.getElementById(`yearSelect`).style.display = `initial`;
+    document.getElementById(`monthSelect`).style.display = `initial`;
+    document.getElementById(`daySelect`).style.display = `initial`;
+    document.getElementById(`histInput`).style.display = `initial`;
+    document.getElementById(`histButton`).style.display = `initial`;
 
 };
